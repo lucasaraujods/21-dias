a982149504f6c35d39dbe9d190f0829fb580f75d
Lucas Araujo <lucasara1707@gmail.com>
2024-02-09 16:57:53 -0300

    adicionando pasta dia 6

---
+++ dia-6/exercicio2.js
@@ -0,0 +1,5 @@
+let i= parseInt(prompt("informe um número"))
+for( let i = 0 ; i<=50; i += 5)
+{
+    console.log(i)
+}
---
+++ dia-6/exercicio3.js
@@ -0,0 +1,5 @@
+let i= parseInt(prompt("informe um número"))
+for(let i = 50 ; i>=0; i -= 5)
+{
+    console.log(i)
+}
